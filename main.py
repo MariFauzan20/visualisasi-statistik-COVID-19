fb05adf0f408ed57bb8f0a7390de3cca1c68b2ce
Mar'i Fauzan <marifauzan20@gmail.com>
2022-12-31 12:36:25 +0700

    Add Feature geoplot

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from os.path import dirname, join
 
 from lineplot import lineplot_tab
 from table import table_tab
+from geoplot import geoplot
 
 
 # Read dataset from file
@@ -17,13 +18,14 @@ covid_data = pd.read_csv("./dataset/covid_19_indonesia_time_series_all.csv")
 covid_data.columns = covid_data.columns.str.replace(" ", "_")
 covid_data.columns= covid_data.columns.str.lower()
 covid_data['date'] = pd.to_datetime(covid_data['date'], format='%m/%d/%Y')
-
 tab1 = lineplot_tab(covid_data)
 
 # covid_data.groupby("date").count()
 
 tab2 = table_tab(covid_data)
-tabs = Tabs(tabs = [tab1, tab2])
+tab3 = geoplot()
+# tabs = Tabs(tabs = [tab1, tab2])
+tabs = Tabs(tabs = [tab1, tab2, tab3])
 
 curdoc().add_root(tabs)
 # show(tabs)
